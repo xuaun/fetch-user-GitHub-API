267e198300963051450654f84d43e82d17142091
João Víctor de Araujo Lima <joaovictor.ne@gmail.com>
2025-02-26 19:46:10 -0300

    Update getWatchers function

--- src/scripts/services/watchers.js
+++ src/scripts/services/watchers.js
@@ -1,10 +1,9 @@
 async function getWatchers(reposResponse) {
-  let allResponses = await Promise.all(
+  const allResponses = await Promise.all(
     reposResponse.map(async (repo) => {
       const url = repo.subscribers_url;
       const response = await fetch(url);
-      const json = await response.json();
-      allResponses.push(json);
+      return await response.json();
     })
   );
 
